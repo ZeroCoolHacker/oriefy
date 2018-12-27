df5ac2b226e47c21f9a6c87f4187e03f738d8ac3
ashir <shahzaibakash@gmail.com>
2018-12-27 08:19:59 -0500

    blog model

--- oriefy/blog/models.py
+++ oriefy/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.utils.translation import gettext as _
 # Create your models here.
+class Blog(models.Model):
+
+    title   = models.CharField(_("Title"), max_length=70)
+    text    = models.CharField(_("Text"), max_length=5000)
+
+    class Meta:
+        verbose_name = _("Blog")
+        verbose_name_plural = _("Blogs")
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("_detail", kwargs={"pk": self.pk})
